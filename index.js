0590240789440a949cff6ec7318275658833bf5b
rebeccalindman <rebeccalindman@icloud.com>
2024-10-17 14:29:12 +0200

    försök på input validering, fastnat på operator

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
+// First number input
 let aVariable = parseFloat(prompt("Enter a number"));
+
+while(isNaN(aVariable)){
+    aVariable = parseFloat(prompt("WRONG INPUT! Enter a number"));
+}
+
+// Operator selection
 let operatorVariable = prompt("Enter operator");
+while((operatorVariable !== "+" && operatorVariable !== "-")){
+    operatorVariable = prompt("Enter operator");
+}
+
+// Second number input
 let bVariable = parseFloat(prompt("Enter another number"));
+while(isNaN(bVariable)){
+    bVariable = parseFloat(prompt("WRONG INPUT! Enter a number"));
+}
+
 let result;
+
 if (operatorVariable == "+"){
     result = aVariable + bVariable;
     alert(`svar: ${result}`);
